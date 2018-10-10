bfd0e8b7c51c6e5815795dac25a3c1ee720ca556
David Galante <davidgalante77@gmail.com>
2018-10-11 01:08:47 +0200

    public Key generation

--- src/Controller.java
+++ src/Controller.java
@@ -10,10 +10,32 @@ public class Controller {
     private TextField aliceATF;
     @FXML
     private TextField bobATF;
+    @FXML
+    private TextField alicePUTF;
+    @FXML
+    private TextField bobPUTF;
+
+    private Cyclic cyclic;
 
     public void setData()
     {
         this.safePrimeCB.getItems().addAll(Cyclic.SAFE_PRIME);
         this.safePrimeCB.setValue(this.safePrimeCB.getItems().get(0));
     }
+
+    public void calculateKeyPairs()
+    {
+        int a=Integer.parseInt(aliceATF.getText());
+        int b=Integer.parseInt(bobATF.getText());
+        int p=Integer.parseInt((String)safePrimeCB.getValue());
+        this.cyclic=new Cyclic();
+        int alicePU=cyclic.calculateKeyPair(a,Cyclic.GENERATOR,p);
+        int bobPU=cyclic.calculateKeyPair(b,Cyclic.GENERATOR,p);
+        this.alicePUTF.setText(""+alicePU);
+        this.bobPUTF.setText(""+bobPU);
+
+
+
+    }
+
 }
